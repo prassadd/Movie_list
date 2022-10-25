9d6891918d2983dafade48b061677d0e39dd800c
prassadd <112117257+prassadd@users.noreply.github.com>
2022-10-26 00:21:41 +0530

    Add files via upload

---
+++ movieees/connect.php
@@ -0,0 +1,18 @@
+<?php
+$conn=new mysqli('localhost','root','','movies');//connection variable to connect with db(any name of variable you want)
+//localhost-server name,default username,password(no spaces),created db
+/*if($conn)//to check if connection is successful
+{
+    echo "connection successful ";
+}
+else{
+    die(mysqli_error($conn));  //passing of variable mandatory 
+}*/
+
+//only keeping error statement once we come to know that connection is successful
+if(!$conn)
+{
+    die(mysqli_error($conn));
+}
+
+?>
